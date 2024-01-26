8fba64b24f549340535e6728268a5f9d2d458d7c
aulainformatica <aulainformatica@UBSCOB313-31.unbosque.edu.co>
2024-01-26 08:40:48 -0500

    Add employes attributes

--- Taller1Corte1/src/co/edu/unbosque/model/EmpleadoDTO.java
+++ Taller1Corte1/src/co/edu/unbosque/model/EmpleadoDTO.java
@@ -1,5 +1,30 @@
 package co.edu.unbosque.model;
 
 public class EmpleadoDTO {
-
+	private long id;
+	private String name;
+	public EmpleadoDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	public EmpleadoDTO(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employe \n"+ "id=" + id + "\n"+"name=" + name + "\n";
+	}
 }
